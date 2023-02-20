7925ea453191c82d5af38503da23e117e4b72791
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-02-20 15:56:44 +0300

    Update HW13.tsx
    h13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,7 +36,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-            setInfo(res.data.errorText)
+            setInfo('')
             setText(res.data.errorText)
                 // дописать
 
@@ -47,18 +47,18 @@ const HW13 = () => {
                     setCode('ошибка 500!')
                     setImage(error500)
                     setText(e.response.data.errorText)
-                    setInfo(e.response.data.errorText)
+                    setInfo('')
 
                 }else if(e.response.status === 400){
                     setCode('Ошибка 400!')
                     setImage(error400)
                     setText(e.response.data.errorText)
-                    setInfo(e.response.data.errorText)
+                    setInfo('')
                 }else {
                     setCode('Error!')
                     setImage(errorUnknown)
                     setText(e.response.data.errorText)
-                    setInfo(e.response.data.errorText)
+                    setInfo('')
                 }
             })
     }
